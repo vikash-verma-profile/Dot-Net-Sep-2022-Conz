8dd0d4c048a590dc8864ad14c426ff04a559da27
Level-Up-4 <vikash-verma-profile>
2022-09-12 16:52:55 +0530

    added code

--- Angular-Labs/sample-project/src/app/utilites/grid-ui/grid-ui.component.ts
+++ Angular-Labs/sample-project/src/app/utilites/grid-ui/grid-ui.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-grid-ui',
@@ -8,7 +8,23 @@ export class GridUiComponent implements OnInit {
 
   constructor() { }
 
+  //getting column names
+  gridColumns: Array<any> = new Array<any>();
+
+  //getting column data
+  gridData: Array<any> = new Array<any>();
+
   ngOnInit(): void {
   }
 
+  @Input("grid-columns")
+  set SetGridColumns(_gridColumn:Array<any>){
+    this.gridColumns=_gridColumn;
+  }
+
+  @Input("grid-data")
+  set SetGridData(_gridData:Array<any>){
+    this.gridData=_gridData;
+  }
 }
+
