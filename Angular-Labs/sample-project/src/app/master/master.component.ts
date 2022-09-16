c3f77c99774b4334816a741e99af6425db32236e
Level-Up-4 <vikash-verma-profile>
2022-09-16 18:06:16 +0530

    added k=jwt

--- Angular-Labs/sample-project/src/app/master/master.component.ts
+++ Angular-Labs/sample-project/src/app/master/master.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginServiceService } from '../services/login-service.service';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MasterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _auth:LoginServiceService) { }
 
   ngOnInit(): void {
   }
 
+  LoggedIn(Input:boolean):boolean{
+    if(Input){
+      return this._auth.logginIn();
+    }
+    else{
+      return !this._auth.logginIn();
+    }
+  }
+  Logout(){
+    this._auth.logoutUser();
+    
+  }
 }
--- Angular-Labs/sample-project/src/app/services/login-service.service.ts
+++ Angular-Labs/sample-project/src/app/services/login-service.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +9,20 @@ export class LoginServiceService {
 
   _loginUrl="https://localhost:44354/api/login/login-user";
   _registerUrl="https://localhost:44354/api/login/register-user";
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient,private _router:Router) { }
 
   loginUser(login:any){
     return this.http.post<any>(this._loginUrl,login);
   }
   getToken(){
     return localStorage.getItem('token');
+  }
+  logginIn(){
+    return !!localStorage.getItem('token');
+  }
+
+  logoutUser(){
+    localStorage.removeItem('token');
+    this._router.navigate(['']);
   }
 }
