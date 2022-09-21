57f1ee9b614f72d7ffc7a7d43fab450d7ebc3ee0
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-21 14:36:22 +0530

    added code

---
+++ UnitTestCase/UnitTestCase/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestCase.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            var encoded= Convert.ToBase64String(Encoding.UTF8.GetBytes("Vikash Verma"));
+            return Ok(new { Value = encoded });
+        }
+    }
+}
