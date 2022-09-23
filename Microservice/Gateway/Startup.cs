253956d7c5f4592e467aa125bb600dcc968b1f45
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-23 17:15:20 +0530

    added code

--- Microservice/Gateway/Startup.cs
+++ Microservice/Gateway/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,7 +27,7 @@ namespace Gateway
         public void ConfigureServices(IServiceCollection services)
         {
            // services.AddControllersWithViews();
-            services.AddOcelot();
+            services.AddOcelot().AddConsul();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
