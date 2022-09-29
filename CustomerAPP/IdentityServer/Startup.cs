0825bd6ee01a4b5320eb72b0758db8b81419174f
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-09-29 18:11:30 +0530

    added code

---
+++ CustomerAPP/IdentityServer/Startup.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var builder = services.AddIdentityServer(x => x.Authentication.CookieLifetime = TimeSpan.FromSeconds(20)).
+                AddInMemoryIdentityResources(Config.IdentityResources).
+                AddInMemoryApiScopes(Config.ApiScopes).AddInMemoryClients(Config.Clients);
+            builder.AddDeveloperSigningCredential();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseIdentityServer();
+        }
+    }
+}
